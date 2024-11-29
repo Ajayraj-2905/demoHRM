900b6bd58fc5d5d24c9c628e0a8ddc5f3c5ea80a
Ajayraj-2905 <i.am.ajayraj29@gmail.com>
2024-11-29 16:06:09 +0530

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -8,10 +8,11 @@ const Login = () => {
   const [password, setPassword] = useState('')
   const navigate = useNavigate()
   const { login } = useAuth()
+  const apiURl = process.env.API_URL
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch('http://localhost:5000/api/auth/login', {
+    const response = await fetch(`${apiURl}api/auth/login`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ username, password }),
