0517b304fd10500256f1f4ab2c4e112dbe01b6df
Ajayraj-2905 <i.am.ajayraj29@gmail.com>
2024-11-29 13:45:59 +0530

    Update 2 in backend package.json

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,6 @@ const app = require('./app')
 const PORT = 5000
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`)
+  console.log(`Server running on port :${PORT}`)
 })
 
