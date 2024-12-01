1f4b485d4af2ce7e78d84ba470f0481c9a76d79a
Ajayraj-2905 <i.am.ajayraj29@gmail.com>
2024-12-01 14:09:39 +0530

    All perfect

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2');
+require('dotenv').config();
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
   password: '',
-  database: 'sample'
+  database: process.env.DB_DATABASE
 })
 
 db.connect((err) => {
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,9 @@
 const app = require('./app')
-const PORT = 5000
+require('dotenv').config();
+
+const PORT = process.env.DB_PORT
 
 app.listen(PORT, () => {
-  console.log(`Server running on port :${PORT}`)
+  console.log(`Server running on http://localhost:${PORT}`)
 })
 
